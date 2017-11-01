ee542951fcafda6357bf3b44397318814420a5fa
stefan2212 <stefan.cernescu94@gmail.com>
2017-11-01 14:36:29 +0200

    Add the layout for the contact page

---
+++ src/app/pages/about-scira/about-scira.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-scira',
+  templateUrl: './about-scira.component.html',
+  styleUrls: ['./about-scira.component.css']
+})
+export class AboutSciraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
